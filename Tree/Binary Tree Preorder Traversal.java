873d7225bed5520da12075c2643c7d612f346bc1
Aditya Nalawade <aditya30n@gmail.com>
2016-04-25 12:09:59 -0700

    Update Binary Tree Preorder Traversal.java

--- Tree/Binary Tree Preorder Traversal.java
+++ Tree/Binary Tree Preorder Traversal.java
@@ -22,6 +22,8 @@ Note: Recursive solution is trivial, could you do it iteratively?
  *     TreeNode(int x) { val = x; }
  * }
  */
+ 
+ // Recursive
 public class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
         return preorderTraversal(root, new ArrayList<>());
@@ -34,6 +36,44 @@ public class Solution {
             preorderTraversal(root.right, preOrder);
         }
         
+        return preOrder;
+    }
+}
+
+// Iterative
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> preOrder = new ArrayList<>();
+        if(root == null)
+            return preOrder;
+        
+        TreeNode current = root;
+        Stack stack = new Stack();
+        stack.add(current);
+        while(!stack.isEmpty()){
+            while(current != null){
+                preOrder.add(current.val);
+                current = current.left;
+                stack.add(current);
+            }
+            
+            current = (TreeNode)stack.pop();
+            
+            if(current != null){
+                current = current.right;
+                stack.push(current);
+            }
+        }
+       
         return preOrder;
     }
 }
