ca194606a8e9709341b1e91010a9e6bb29b9adef
Aditya Nalawade <aditya30n@gmail.com>
2016-04-29 10:26:31 -0700

    Update bit_Reverse String.java

--- bit_Reverse String.java
+++ bit_Reverse String.java
@@ -5,6 +5,23 @@ Example:
 Given s = "hello", return "olleh".
 */
 
+//No extra memory
+public class Solution {
+    public String reverseString(String s) {
+        int i = 0;
+        int j = s.length()-1;
+        char[] chars = s.toCharArray();
+        for (i=0; i<j; i++, j--){
+            chars[i] ^= chars[j];
+            chars[j] ^= chars[i];
+            chars[i] ^= chars[j];
+        }
+        
+        return new String(chars);
+    }
+}
+
+// Using 1 char space
 public class Solution {
     public String reverseString(String s) {
         if(s == null || s.length() <= 1)
