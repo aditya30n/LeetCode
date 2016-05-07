46cdb739a02d76a31bcb49c3ff4682178d571f6a
Aditya Nalawade <aditya30n@gmail.com>
2016-05-06 22:32:45 -0700

    Update Balanced Binary Tree.java

--- Tree/Balanced Binary Tree.java
+++ Tree/Balanced Binary Tree.java
@@ -13,6 +13,28 @@ For this problem, a height-balanced binary tree is defined as a binary tree in w
  *     TreeNode(int x) { val = x; }
  * }
  */
+ 
+ // O(n)
+public class Solution {
+    public boolean isBalanced(TreeNode root) {
+        return height(root) != -1;
+    }
+    
+    private int height(TreeNode node){
+        if(node == null)
+            return 0;
+        
+        int left = height(node.left);
+        int right = height(node.right);
+        
+        if(Math.abs(left - right)> 1 || left == -1 || right == -1)
+            return -1;
+        return 1 + Math.max(left, right);
+    }
+}
+ 
+ 
+ // O(n^2)
 public class Solution {
     public boolean isBalanced(TreeNode root) {
         if(root == null)
